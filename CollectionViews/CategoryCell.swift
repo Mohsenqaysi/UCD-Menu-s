41a391b81914884588a5e5a913084bfad6a8a170
mohsenqaysi <mohsen.qaysi@ucdconnect.ie>
2017-12-07 17:16:08 +0000

    The allergies cell is working

--- CollectionViews/CategoryCell.swift
+++ CollectionViews/CategoryCell.swift
@@ -8,7 +8,72 @@
 
 import UIKit
 
-class CategoryCell: UICollectionViewCell {
+class CategoryCell: UICollectionViewCell, UICollectionViewDataSource, UICollectionViewDelegate,UICollectionViewDelegateFlowLayout {
+
+    let CellID = "CellID"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    let restaurantCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let view = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        view.translatesAutoresizingMaskIntoConstraints = false // Very important
+        view.backgroundColor = .blue
+        return view
+    }()
+    
+    let nameLable: UILabel = {
+        let lable = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
+        lable.text = "CategoryCell Name Lable"
+        return lable
+    }()
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupViews() {
+        backgroundColor = .white
+
+        
+        //MARK: add views the the root view:
+        addSubview(restaurantCollectionView)
+        addSubview(nameLable)
+        
+        restaurantCollectionView.dataSource = self
+        restaurantCollectionView.delegate = self
+        
+        // MARK: Register the cell:
+        restaurantCollectionView.register(AlergiesCell.self, forCellWithReuseIdentifier: CellID)
+        
+        // MARK: add constraints to the views:
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-8-[v0]-8-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": restaurantCollectionView]))
+            
+         addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-50-[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": restaurantCollectionView]))
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CellID, for: indexPath) as! AlergiesCell
+        return collectionView.dequeueReusableCell(withReuseIdentifier: CellID, for: indexPath) 
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 150, height: frame.height)
+    }
+}
+
+class AlergiesCell: UICollectionViewCell {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -20,6 +85,6 @@ class CategoryCell: UICollectionViewCell {
     }
     
     func setupViews() {
-        backgroundColor = .red
+        backgroundColor = .yellow
     }
 }
